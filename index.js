9dba808ffee3034a250c2283cd60ceda78bbffd0
salem701osama-lang <salem701osama@gmail.com>
2026-06-03 16:58:47 +0300

    صلحت البورت

--- index.js
+++ index.js
@@ -35,4 +35,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`الشات شغال على ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`شغال على ${PORT}`));
