7173520cbc1e006b7bc277520792acee8bd91cae
salem701osama-lang <salem701osama@gmail.com>
2026-06-03 19:29:53 +0300

    تقييمات

--- index.js
+++ index.js
@@ -6,39 +6,49 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  },
+  cors: { origin: "*" },
   transports: ['websocket', 'polling']
 });
 
 app.use(express.static(path.join(__dirname)));
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public-index.html'));
 });
 
 let onlineUsers = {};
+let chatHistory = []; // هنخزن الرسايل هنا
+const MAX_MESSAGES = 2000; // عايز 1000 غير الرقم ده
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
-  
+
   socket.on('join', (username) => {
     onlineUsers[socket.id] = { id: socket.id, name: username };
+
+    // ابعت تاريخ الشات كله للي دخل جديد
+    socket.emit('chatHistory', chatHistory);
+
     io.emit('userList', Object.values(onlineUsers).map(u => u.name));
     io.emit('chat', { user: 'النظام', text: username + ' دخل الدردشة 🔥', time: new Date().toLocaleTimeString('ar-EG', {hour: '2-digit', minute:'2-digit'}) });
   });
 
-  socket.on('chat', (msg) => {
+  socket.on('chat', (data) => {
     const user = onlineUsers[socket.id];
     if(user) {
-      io.emit('chat', { 
-        user: user.name, 
-        text: msg,
+      const msg = {
+        user: user.name,
+        text: data.text || '',
+        img: data.img || null,
         time: new Date().toLocaleTimeString('ar-EG', {hour: '2-digit', minute:'2-digit'}),
         id: socket.id
-      });
+      };
+
+      // خزن الرسالة
+      chatHistory.push(msg);
+      // لو عدينا 2000 امسح أقدم واحدة
+      if(chatHistory.length > MAX_MESSAGES) chatHistory.shift();
+
+      io.emit('chat', msg);
     }
   });
 
