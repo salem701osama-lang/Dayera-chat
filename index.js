3028d555f1b091ea72bde2e8c815c0e12a05062c
salem701osama-lang <salem701osama@gmail.com>
2026-06-03 19:46:07 +0300

    تطوير

--- index.js
+++ index.js
@@ -16,39 +16,45 @@ app.get('/', (req, res) => {
 });
 
 let onlineUsers = {};
-let chatHistory = []; // هنخزن الرسايل هنا
-const MAX_MESSAGES = 2000; // عايز 1000 غير الرقم ده
+let chatHistory = [];
+const MAX_MESSAGES = 2000;
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
   socket.on('join', (username) => {
     onlineUsers[socket.id] = { id: socket.id, name: username };
-
-    // ابعت تاريخ الشات كله للي دخل جديد
     socket.emit('chatHistory', chatHistory);
-
     io.emit('userList', Object.values(onlineUsers).map(u => u.name));
-    io.emit('chat', { user: 'النظام', text: username + ' دخل الدردشة 🔥', time: new Date().toLocaleTimeString('ar-EG', {hour: '2-digit', minute:'2-digit'}) });
+    io.emit('chat', { user: 'النظام', text: username + ' دخل الدردشة 🔥', time: new Date().toLocaleTimeString('ar-EG', {hour: '2-digit', minute:'2-digit'}), msgId: Date.now() });
   });
 
   socket.on('chat', (data) => {
     const user = onlineUsers[socket.id];
     if(user) {
       const msg = {
+        msgId: Date.now() + Math.random(), // ID للمسح
         user: user.name,
         text: data.text || '',
         img: data.img || null,
+        audio: data.audio || null, // ← الريكورد
         time: new Date().toLocaleTimeString('ar-EG', {hour: '2-digit', minute:'2-digit'}),
         id: socket.id
       };
 
-      // خزن الرسالة
       chatHistory.push(msg);
-      // لو عدينا 2000 امسح أقدم واحدة
       if(chatHistory.length > MAX_MESSAGES) chatHistory.shift();
 
       io.emit('chat', msg);
+    }
+  });
+
+  // مسح الرسالة
+  socket.on('deleteMsg', (msgId) => {
+    const user = onlineUsers[socket.id];
+    if(user) {
+      chatHistory = chatHistory.filter(m => m.msgId!= msgId);
+      io.emit('msgDeleted', msgId); // بلغ الكل ان الرسالة اتمسحت
     }
   });
 
@@ -57,7 +63,7 @@ io.on('connection', (socket) => {
     if(user) {
       delete onlineUsers[socket.id];
       io.emit('userList', Object.values(onlineUsers).map(u => u.name));
-      io.emit('chat', { user: 'النظام', text: user.name + ' خرج من الدردشة', time: '' });
+      io.emit('chat', { user: 'النظام', text: user.name + ' خرج من الدردشة', time: '', msgId: Date.now() });
     }
   });
 });
